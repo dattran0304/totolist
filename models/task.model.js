e6887e604a0929055a9642c20b3cfa5414b05330
dattq0304 <dattq.it@gmail.com>
2023-02-12 11:52:14 +0700

    update

--- models/task.model.js
+++ models/task.model.js
@@ -4,19 +4,16 @@ const Schema = mongoose.Schema;
 //TO FIX THIS WARNING: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7 ...
 mongoose.set("strictQuery", false)
 
-// Connect to the database
-/* local
+// Connect to the local db
+/* 
 mongoose.connect('mongodb://127.0.0.1/todolist', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 });
 */
-//railway
-const MONGOHOST = 'containers-us-west-195.railway.app'
-const MONGOPASSWORD = '77WvuJcng0hDeo5bLIHw'
-const MONGOPORT = '7958'
-const MONGOUSER = 'mongo'
-mongoose.connect(`mongodb://${{ MONGOUSER }}:${{ MONGOPASSWORD }}@${{ MONGOHOST }}:${{ MONGOPORT }}`, {
+
+//Connect to the railway db
+mongoose.connect('mongodb://mongo:77WvuJcng0hDeo5bLIHw@containers-us-west-195.railway.app:7958', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 });
@@ -24,9 +21,9 @@ mongoose.connect(`mongodb://${{ MONGOUSER }}:${{ MONGOPASSWORD }}@${{ MONGOHOST 
 const TaskSchema = new Schema({
 	title: String
 }, {
-	collection: 'tasks'
-});
+	collection: 'todolist'
+});	
 
-const TaskModel = mongoose.model('tasks', TaskSchema);
+const TaskModel = mongoose.model('todolis', TaskSchema);
 
 module.exports = TaskModel;

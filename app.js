9df8c215ec07e7bbfed4149d615fb5f5fc2dd12a
dattq0304 <dattq.it@gmail.com>
2023-02-12 11:55:03 +0700

    update

--- app.js
+++ app.js
@@ -19,5 +19,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/api/task', TaskRouter)
 
 app.listen(port, () => {
-	console.log(`App listening at http://localhost:${port}`)
+	//console.log(`App listening at http://localhost:${port}`)
+	console.log('App listening')
 })
